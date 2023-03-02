681da964a5d9999d496ccc47e7c3ebf86bff7b60
Dani Bagley <bagley.a.danielle@gmail.com>
2023-03-02 11:59:43 -0700

    Fix date

--- src/ArticleDetails/ArticleDetails.js
+++ src/ArticleDetails/ArticleDetails.js
@@ -7,13 +7,15 @@ const ArticleDetails = ({ articles }) => {
 
   const found = articles[id];
 
+  const published = found.published_date.split("T");
+
   return (
     <div className="details">
       <h1 className="detailsTitle">{found.title}</h1>
       <img className="detailsImg" src={found.multimedia[1].url} />
       <p className="info">{found.abstract}</p>
       <p className="info">{found.byline}</p>
-      <p className="info">{found.published_date}</p>
+      <p className="info">Published: {published[0]}</p>
       <Link to={found.short_url}>See Article</Link>
     </div>
   );
